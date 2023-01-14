6281ecce32f0afedcc610ea6aa3375f0c425e63d
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-14 12:29:34 -0500

    added edit password functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
       Current.user = User.find_by(id: session[:user_id])
     end
   end
+
+  def require_login
+    redirect_to sign_in_path, alert: 'You must be logged in to access this section.' if Current.user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/destroy'
+  get 'passwords/edit'
+  get 'passwords/update'
   get 'about', to: 'about#index'
+
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
