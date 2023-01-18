a33d728c22bb4d9fbed2ff53e7eba0433a7efa78
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-18 16:39:35 -0500

    changed notices

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,5 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    Rails.logger.info auth
-
     twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
     twitter_account.update(
       name: auth.info.name,
@@ -9,8 +7,7 @@ class OmniauthCallbacksController < ApplicationController
       token: auth.credentials.token,
       secret: auth.credentials.secret
     )
-
-    redirect_to twitter_accounts_path, notice: 'Successfully linked your Twitter account'
+    redirect_to twitter_accounts_path, notice: 'Successfully linked Twitter account'
   end
 
   def auth
