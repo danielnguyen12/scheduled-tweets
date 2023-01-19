08b60142b1666ff70eea60038f8572b5ee53af8e
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-19 13:59:04 -0500

    added functionality to edit and destroy scheduled Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_login
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -12,13 +13,33 @@ class TweetsController < ApplicationController
   def create
     @tweet = Current.user.tweets.new(tweet_params)
     if @tweet.save
-      redirect_to tweets_path, notice: 'Succesfully scheduled tweet'
+      redirect_to tweets_path, notice: 'Succesfully scheduled Tweet'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: 'Successfully updated Tweet'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: 'Scheduled Tweet was succesfully deleted'
+  end
+
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
