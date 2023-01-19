b026484a37e037730260e4299524fb405ac6866c
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-19 10:34:05 -0500

    created partial for form errors, refactored views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,17 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to @tweet, notice: 'Succesfully scheduled tweet'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
 end
